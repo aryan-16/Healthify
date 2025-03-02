47eee7ff238999b84deb418cbb5f1dbb82747920
aryan-16 <129524809+aryan-16@users.noreply.github.com>
2025-03-02 23:32:20 +0530

    RecyclerDB Commit

---
+++ app/src/main/java/com/example/healthify/Adapters/RunAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.healthify.Adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.healthify.db.Run
+
+class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>(){
+
+    inner class RunViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+
+    val diffCallback = object : DiffUtil.ItemCallback<Run>(){
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+              return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+        return oldItem.hashCode() == newItem.hashCode()
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this , diffCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
